c175cb20c2493ff170b0e42480bee59c9a09f311
origenal <atoka2001@gmail.com>
2022-12-02 16:33:54 +0200

    After correct some conflicts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,29 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-<<<<<<< Updated upstream
 
-=======
+
+
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent }
  
->>>>>>> Stashed changes
-
-const routes: Routes = [
-   {path: 'Login' , component: LoginComponent}
- 
-  
-];
-
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-<<<<<<< Updated upstream
+
 export const routingComponents =[LoginComponent  ]
-=======
-export const routingComponents = [LoginComponent]
->>>>>>> Stashed changes
+
+
+
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,12 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     LoginComponent
-<<<<<<< Updated upstream
    
     
-=======
-
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
