5ef9c79093ad3769a501a58796e4418c7fbefc2a
bassantyasser043 <bassantmohamed043@gmail.com>
2022-12-19 04:19:24 +0200

    connection

--- Backend/src/main/java/com/example/e_store/controller/ProductController.java
+++ Backend/src/main/java/com/example/e_store/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.e_store.controller;
 import com.example.e_store.dto.ProductRequest;
 import com.example.e_store.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/product")
 public class ProductController {
@@ -25,7 +27,8 @@ public class ProductController {
     )
     public ResponseEntity<?> createProduct(
             @RequestPart("product") ProductRequest productRequest,
-            @RequestPart("image") MultipartFile image) {
+            @RequestPart("imageFile") MultipartFile image) {
+        log.info("Adding New Product To the DB {}", productRequest.getProductId());
         productService.save(productRequest, image);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
