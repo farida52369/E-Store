c087d7a0b34fb96ac6ac89787b2c00508516a746
origenal <atoka2001@gmail.com>
2022-12-20 19:23:56 +0200

    add products boxes in the home page

--- Frontend/src/app/add-item/add-item.component.ts
+++ Frontend/src/app/add-item/add-item.component.ts
@@ -1,30 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductRequest } from '../dto/data';
 import { AddItemService } from './add-item.service';
-
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
-  styleUrls: ['./add-item.component.css']
+  styleUrls: ['./add-item.component.css'],
 })
 export class AddItemComponent {
-
-  constructor(private service: AddItemService) { }
+  constructor(private service: AddItemService, private router:Router) {}
 
   imageToBeUploaded!: File;
-
+  image: any;
 
   public onFileChange(event: any) {
-    this.imageToBeUploaded = event.target.files[0];
-  }
+    const files = event.target.files;
 
+    if (files.length === 0) return;
+
+    this.imageToBeUploaded = files[0];
+
+    // No need for that check _ already handled ..
+    // const mimeType = files[0].type;
+    // if (mimeType.match(/image\/*/) == null) {
+    //     return;
+    // }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.image = reader.result;
+    };
+  }
 
   public addItem() {
     const title = (<HTMLInputElement>document.getElementById('title')).value;
-    const price: number = parseFloat((<HTMLInputElement>document.getElementById('price')).value);
-    const quantity = parseInt((<HTMLInputElement>document.getElementById('quantity')).value);
+    const price: number = parseFloat(
+      (<HTMLInputElement>document.getElementById('price')).value
+    );
+    const quantity = parseInt(
+      (<HTMLInputElement>document.getElementById('quantity')).value
+    );
     const category = (<HTMLInputElement>document.getElementById('c')).value;
     const desc = (<HTMLInputElement>document.getElementById('desc')).value;
 
@@ -34,32 +52,36 @@ export class AddItemComponent {
       price: price,
       category: category,
       inStock: quantity,
-      description: desc
-    }
+      description: desc,
+    };
 
     const formParams = new FormData();
-    formParams.append('imageFile', new Blob([this.imageToBeUploaded], {
-      type: 'multipart/form-data'
-    }), this.imageToBeUploaded.name);
-    formParams.append('product', new Blob([JSON.stringify(product)], {
-      type: 'application/json'
-    }))
-    console.log("form params => " + formParams.get("product"))
-    console.log("form params => " + formParams.get("imageFile"))
+    formParams.append(
+      'imageFile',
+      new Blob([this.imageToBeUploaded], {
+        type: 'multipart/form-data',
+      }),
+      this.imageToBeUploaded.name
+    );
+    formParams.append(
+      'product',
+      new Blob([JSON.stringify(product)], {
+        type: 'application/json',
+      })
+    );
+    console.log('form params => ' + formParams.get('product'));
+    console.log('form params => ' + formParams.get('imageFile'));
 
     this.service.createProduct(formParams).subscribe(
       (res) => {
-        console.log("YUUUUUUUUUUUUP");
-      }, (error) => {
-        console.log("YARABBBBBBBBBBB")
+        console.log('YUUUUUUUUUUUUP');
+        this.router.navigate(['/home'], {
+          queryParams: { inHome: 'true' },
+        });
+      },
+      (error) => {
+        console.log('YARABBBBBBBBBBB');
       }
-    )
+    );
   }
-
-
 }
-
-
-
-
-
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private authService: AuthService
   ) {}
+  products: any;
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe((params) => {
@@ -19,6 +20,7 @@ export class HomeComponent implements OnInit {
         this.updateStatus();
       }
     });
+    this.showProducts();
   }
 
   updateStatus(): void {
@@ -32,7 +34,65 @@ export class HomeComponent implements OnInit {
       '500px';
     (<HTMLInputElement>document.getElementById('logout')).style.display =
       'block';
+    (<HTMLInputElement>document.getElementById('add-product')).style.display =
+     'block';
   }
+
+  showProducts() {
+    this.products = [{ image: '../../assets/bike.jpg', price: 1000, name: 'Bike', id: '1' },
+    { image: '../../assets/shoes.jpg', price: 29.99, name: 'Shoes', id:'2' },
+    { image: '../../assets/bear.jpg', price: 9.99, name: 'Bear', id:'3'},
+    { image: '../../assets/laptop.jpg', price: 10000, name: 'Laptop', id:'4' },
+    {image: '../../assets/cleancode.jpg', price: 10, name: 'Clean Code', id: '5'},
+    { image: '../../assets/phone.jpg', price: 100, name: 'Phone', id:'6' },
+    { image: '../../assets/tv.jpg', price: 500, name: 'Television', id:'7' },
+  {image: '../../assets/motorcycle.jpg', price: 5000, name: 'Motorcycle', id: '8'}];
+    let elem = document.getElementById('products')
+    for (let i = 0; i < this.products.length; i++) {
+      this.buildCard(this.products[i]);
+    }
+  }
+  
+  buildCard(product: any){
+    let e = document.createElement('div');
+    e.setAttribute('style', 'box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); min-width: 300px;margin: auto; text-align: center; max-width: 500px;');
+    e.setAttribute('id', product.id);
+    e.appendChild(this.buildImage(product));
+    e.appendChild(this.buildHeader(product));
+    e.appendChild(this.buildPrice(product));
+    e.appendChild(this.buildButton(product));
+    document.getElementById('products')?.appendChild(e);
+    return e;
+  }
+
+  buildImage(product: any) {
+    let i = document.createElement('img');
+    i.setAttribute('src', product.image);
+    i.setAttribute('style', '  width: 100%;height: 200px;');
+    return i;
+  }
+
+  buildHeader(product: any) {
+    let h = document.createElement('h1');
+    h.innerText = product.name;
+    return h;
+  }
+
+  buildPrice(product: any) {
+    let p = document.createElement('p');
+    p.setAttribute('style', 'color: grey;font-size: 22px;');
+    p.innerText = `$${product.price}`;
+    return p;
+  }
+
+  buildButton(product: any) {
+    let b = document.createElement('button');
+    let a = document.createElement('a');
+    b.setAttribute('style', 'border: none;outline: 0;padding: 12px;color: white;background-color: #000;text-align: center;cursor: pointer;width: 100%;font-size: 18px;')
+    b.innerText = 'Add to Cart';
+    return b;
+  }
+
 
   logOut() {
     this.authService.logout();
